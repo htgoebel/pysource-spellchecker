6e3949bf0684c11ebbb8fcbb24bf639bfe168e76
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-07-04 12:41:44 +0200

    Add test file.

---
+++ test/data/some.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This is the docstring
+"""
+
+# this is a comment
+u'This is a unicode string'
+'a' # one character
+u'\xfc' # one unicode character (u-umlaut)
+
+import lib2to3.main
+import argparse
