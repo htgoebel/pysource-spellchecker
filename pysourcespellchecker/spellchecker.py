fe4645a28a1da3cc06f6f8b4d20ebdabedceb709
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-07-04 14:07:51 +0200

    Add a filter for she-bang words.
    Bug: As this is a filter, it does not honour the position and would accept
    all words looking like a she-bank line.

--- pysourcespellchecker/spellchecker.py
+++ pysourcespellchecker/spellchecker.py
@@ -3,24 +3,42 @@ Spellcheck texts using `enchant`.
 """
 
 import sys
+import re
 
 import enchant
 from enchant.checker import SpellChecker as _SpellChecker
 from enchant.checker.CmdLineChecker import CmdLineChecker
 from enchant.tokenize import get_tokenizer
-from enchant.tokenize import EmailFilter, URLFilter
+from enchant.tokenize import Filter, EmailFilter, URLFilter
 
 __author__ = "Hartmut Goebel <h.goebel@crazy-compilers.com>"
 __copyright__ = "Copyright 2012 by Hartmut Goebel <h.goebel@crazy-compilers.com>"
 __licence__ = "GNU Public Licence v3 (GPLv3)"
 
 
+class SheBangFilter(Filter):
+    """Filter skipping over shebang lines for the python interpreter.
+    This filter skips any words matching the following regular expression:
+
+           ^#!/.+python.*$
+
+    That is, any words that resemble email addresses.
+    """
+    _pattern = re.compile(r"^#!/.+$")
+    def _skip(self,word):
+        if self._pattern.match(word):
+            return True
+        return False
+
+
+filters_to_use = [EmailFilter,URLFilter,SheBangFilter]
+
 class SpellChecker(object):
     def __init__(self, language, pwl=None):
         if pwl:
             language = enchant.DictWithPWL(language, pwl)
         self._checker = _SpellChecker(lang=language,
-                                      filters=[EmailFilter,URLFilter])
+                                      filters=filters_to_use)
 
     def check(self, text):
         self._checker.set_text(text)
@@ -34,7 +52,7 @@ class CmdLineSpellChecker(object):
         if pwl:
             language = enchant.DictWithPWL(language, pwl)
         self._checker = _SpellChecker(lang=language,
-                                      filters=[EmailFilter,URLFilter])
+                                      filters=filters_to_use)
         self.cmdln = CmdLineChecker()
         self.cmdln.set_checker(self._checker)
 
--- test/data/some.py
+++ test/data/some.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 """
 This file is part of pysource-spellchecker.
 
 This word should be in project.dict: ixelbrings
 This word should not be in project.dict: mixelmux
 """
+
+# The next line intentionally contains a typo behind the `--` to
+# trigger the spell checker.
+#!/usr/bin/env python -- dummy shebanG word, will be skipped, too :-(
+#!/usr/bin/env perl -- not for python, will not be skipped
+
 
 # this is a comment
 u'This is a unicode string'
@@ -13,3 +19,4 @@ u'\xfc' # one unicode character (u-umlaut)
 
 import lib2to3.main
 import argparse
+
